284306bd06215ec324ccd8a211f113bc003ca0e5
Gno <1570761182@qq.com>
2017-07-09 10:50:00 +0800

    vector and iterator

---
+++ 2017-07-09/vector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<iterator>
+#include<ostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void showValue(const int &i)
+{
+    cout<<i<<endl;
+    return ;
+}
+int main()
+{
+    vector<int> ves(5);
+    for(int i = 0;i<5;i++)
+        ves[i] = i;
+    vector<int>::iterator it = ves.begin();
+    cout<<"iterator print"<<endl;
+    while(it!=ves.end())
+        cout<<*it++<<endl;
+    cout<<"for_each print"<<endl;
+    for_each(ves.begin(),ves.end(),showValue);
+    random_shuffle(ves.begin(),ves.end());
+    cout<<"random print"<<endl;
+    for_each(ves.begin(),ves.end(),showValue);
+    cout<<"sort print"<<endl;
+    sort(ves.begin(),ves.end());
+    for_each(ves.begin(),ves.end(),showValue);
+    cout<<"reverse_iterator print"<<endl;
+    for_each(ves.rbegin(),ves.rend(),showValue);
+    cout<<"ostream_iterator print"<<endl;
+    ostream_iterator<int,char> out_iter(cout," ");
+    copy(ves.begin(),ves.end(),out_iter);
+    cout<<endl;
+    return 0;
+}
